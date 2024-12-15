15258590438207a3b0b787f2d9d0357e4d2cfd35
The Dude <marvellousdvd@gmail.com>
2024-12-15 05:53:52 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './blogDetails';
 
 
 function App() {
@@ -10,12 +11,24 @@ function App() {
   
   return (
     <>
+    <Router>
     <div className="App">
       <Navbar />
         <div className="content">
-          <Home /> 
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
         </div>
       </div>
+    </Router>
     </>
   )
 }
